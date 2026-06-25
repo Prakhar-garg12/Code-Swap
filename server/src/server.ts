6d5b9572871692a083d1bad7a150de3fd588f84f
Prakhar Garg <satishagrawal6993@gmail.com>
2026-06-25 22:40:21 +0530

    redo

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,8 +15,7 @@ app.use(express.json())
 
 // app.use(cors())
 app.use(cors({
-    // origin: "http://localhost:5173", // Allow frontend access
-	origin : "https://code-swap.onrender.com/",
+    origin: "http://localhost:5173", // Allow frontend access
     credentials: true
 }));
 
